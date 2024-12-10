cded61c3c848ad8e4171b1830c63a15042ed8c6f
enjoys <dealy.info@gmail.com>
2024-12-10 18:33:46 +0300

    brands stub

--- src/Admin/Brands/Controller.php
+++ src/Admin/Brands/Controller.php
@@ -8,7 +8,11 @@ namespace EnjoysCMS\Module\Catalog\Admin\Brands;
 
 use EnjoysCMS\Core\Routing\Annotation\Route;
 use EnjoysCMS\Module\Catalog\Admin\AdminController;
+use EnjoysCMS\Module\Catalog\Repository\VendorRepository;
 use Psr\Http\Message\ResponseInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * TODO
@@ -16,15 +20,23 @@ use Psr\Http\Message\ResponseInterface;
 #[Route('admin/catalog/brand', '@catalog_brand_')]
 final class Controller extends AdminController
 {
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route(
         path: 's',
         name: 'list',
         comment: 'Список брендов'
     )]
-    public function list(): ResponseInterface
+    public function list(VendorRepository $vendorRepository): ResponseInterface
     {
-
-        return $this->response('');
+        return $this->response(
+            $this->twig->render($this->templatePath . '/brands.twig', [
+                'brands' => $vendorRepository->findAll()
+            ])
+        );
     }
 
 
