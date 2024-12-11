e109ff03dc8e6a7fd695aa2bf7a97d917f94d758
enjoys <dealy.info@gmail.com>
2024-12-11 10:23:28 +0300

    wishlist view

--- src/Controller/Wishlist.php
+++ src/Controller/Wishlist.php
@@ -2,24 +2,70 @@
 
 namespace EnjoysCMS\Module\Catalog\Controller;
 
+use Countable;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
-use EnjoysCMS\Core\AbstractController;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use EnjoysCMS\Core\Auth\Identity;
+use EnjoysCMS\Core\Exception\NotFoundException;
+use EnjoysCMS\Core\Pagination\Pagination;
 use EnjoysCMS\Core\Routing\Annotation\Route;
 use EnjoysCMS\Module\Catalog\Repository\WishlistRepository;
 use Exception;
+use InvalidArgumentException;
+use Iterator;
 use Psr\Http\Message\ResponseInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 #[Route('/catalog/wishlist', name: 'catalog_wishlist_', priority: 3)]
-class Wishlist extends AbstractController
+class Wishlist extends PublicController
 {
 
-    #[Route(name: 'view')]
-    public function view(WishlistRepository $wishlistRepository, EntityManagerInterface $em, Identity $identity): ResponseInterface
-    {
-        $wishlist = $wishlistRepository->findBy(['user' => $identity->getUser()]);
-        dd($wishlist);
-        return $this->json(json_encode($wishlist));
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     * @throws NotFoundException
+     */
+    #[Route(path: '@{page}', name: 'view', requirements: ['page' => '\d+'], defaults: ['page' => 1], methods: ['GET'])]
+    public function view(
+        WishlistRepository $wishlistRepository,
+        Identity $identity
+    ): ResponseInterface {
+        $template_path = '@m/catalog/wishlist.twig';
+        if (!$this->twig->getLoader()->exists($template_path)) {
+            $template_path = __DIR__ . '/../../template/wishlist.twig';
+        }
+        $pagination = new Pagination(
+            $this->request->getAttribute('page', 1),
+            $this->config->getPerPage()
+        );
+
+        $qb = $wishlistRepository->getAllProductsQueryBuilder($identity->getUser());
+        $qb->setFirstResult($pagination->getOffset())
+            ->setMaxResults(
+                $pagination->getLimitItems()
+            );
+
+        /** @var Iterator<\EnjoysCMS\Module\Catalog\Entity\Wishlist>&Countable $result */
+        $result = new Paginator($qb->getQuery());
+        $pagination->setTotalItems($result->count());
+
+
+        return $this->response(
+            $this->twig->render(
+                $template_path,
+                [
+                    'pagination' => $pagination,
+                    'config' => $this->config,
+                    'setting' => $this->setting,
+                    'breadcrumbs' => $this->breadcrumbs,
+                    'wishlists' => $result
+                ]
+            )
+        );
     }
 
     #[Route(path: '/count', name: 'count')]
@@ -32,19 +78,26 @@ class Wishlist extends AbstractController
      * @throws Exception
      */
     #[Route('/add', name: 'add_or_remove')]
-    public function addOrRemove(EntityManagerInterface $em, WishlistRepository $wishlistRepository, \EnjoysCMS\Module\Catalog\Repository\Product $productRepository, Identity $identity): ResponseInterface
-    {
+    public function addOrRemove(
+        EntityManagerInterface $em,
+        WishlistRepository $wishlistRepository,
+        \EnjoysCMS\Module\Catalog\Repository\Product $productRepository,
+        Identity $identity
+    ): ResponseInterface {
         $user = $identity->getUser();
 
         if ($user->isGuest()) {
-            return $this->json(['error' => true, 'message' => 'Для добавления товара в избранное - необходимо авторизоваться'], 400);
+            return $this->json(
+                ['error' => true, 'message' => 'Для добавления товара в избранное - необходимо авторизоваться'],
+                400
+            );
         }
 
         $parsedBody = json_decode($this->request->getBody()->getContents());
 
         try {
-            $product = $productRepository->find($parsedBody->productId ?? throw new \InvalidArgumentException());
-        }catch (\InvalidArgumentException) {
+            $product = $productRepository->find($parsedBody->productId ?? throw new InvalidArgumentException());
+        } catch (InvalidArgumentException) {
             $product = null;
         }
 
@@ -61,7 +114,7 @@ class Wishlist extends AbstractController
         }
 
         $wishlistProduct = new \EnjoysCMS\Module\Catalog\Entity\Wishlist();
-        $wishlistProduct->setCreatedAt(new \DateTimeImmutable());
+        $wishlistProduct->setCreatedAt(new DateTimeImmutable());
         $wishlistProduct->setUser($user);
         $wishlistProduct->setProduct($product);
         $wishlistProduct->setProductOptions([]);
@@ -70,7 +123,6 @@ class Wishlist extends AbstractController
         $em->flush();
 
         return $this->json(sprintf('%s добавлен в избранное', $product->getName()));
-
     }
 
 }
--- src/Repository/WishlistRepository.php
+++ src/Repository/WishlistRepository.php
@@ -2,8 +2,11 @@
 
 namespace EnjoysCMS\Module\Catalog\Repository;
 
-use EnjoysCMS\Module\Catalog\Entity\Wishlist;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use EnjoysCMS\Core\Users\Entity\User;
+use EnjoysCMS\Module\Catalog\Entity\Wishlist;
 
 /**
  * @method Wishlist|null find($id, $lockMode = null, $lockVersion = null)
@@ -13,4 +16,19 @@ use Doctrine\ORM\EntityRepository;
  */
 class WishlistRepository extends EntityRepository
 {
+    public function getAllProductsQueryBuilder(User $user): QueryBuilder
+    {
+        return $this->createQueryBuilder('w')
+            ->select('w', 'p', 'c', 't', 'i', 'm', 'u', 'q', 'pr')
+            ->leftJoin('w.product', 'p')
+            ->leftJoin('p.category', 'c')
+            ->leftJoin('c.parent', 't')
+            ->leftJoin('p.meta', 'm')
+            ->leftJoin('p.urls', 'u')
+            ->leftJoin('p.quantity', 'q')
+            ->leftJoin('p.prices', 'pr')
+            ->leftJoin('p.images', 'i', Join::WITH, 'i.product = p.id')
+            ->where('w.user = :user')
+            ->setParameter('user', $user);
+    }
 }
